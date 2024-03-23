6915c091f09f86741f8e7ad178a925d1c8560528
Ejder Dag <ejdrdag@gmail.com>
2024-03-23 17:13:42 +0300

    Initial Commit

---
+++ Budget-Tracking-SwiftUI/Budget_Tracking_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budget_Tracking_SwiftUIApp.swift
+//  Budget-Tracking-SwiftUI
+//
+//  Created by Ejder Dağ on 23.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Budget_Tracking_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Budget-Tracking-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Budget-Tracking-SwiftUI
+//
+//  Created by Ejder Dağ on 23.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
