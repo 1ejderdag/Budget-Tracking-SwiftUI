bc190b4a71953b52ff7ce370e2917c5b5bfa2867
Ejder Dag <ejdrdag@gmail.com>
2024-03-23 23:42:45 +0300

    design login screen

--- Budget-Tracking-SwiftUI/ContentView.swift
+++ Budget-Tracking-SwiftUI/ContentView.swift
@@ -8,16 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack {
+                Color.orange
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundStyle(.white.opacity(0.15))
+                Circle()
+                    .scale(1.35)
+                    .foregroundStyle(.white)
+                
+                VStack {
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width: 320, height: 50)
+                        .background(Color.black.opacity(0.08))
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .border(Color.red, width: CGFloat(wrongUsername))
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 320, height: 50)
+                        .background(Color.black.opacity(0.08))
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .border(Color.red, width: CGFloat(wrongPassword))
+                    Button("Login") {
+                        // Authenticate User
+                        authenticateUser(username: username, password: password)
+                    }
+                    .foregroundStyle(.white)
+                    .frame(width: 320, height: 50)
+                    .background(Color.orange)
+                    .clipShape(RoundedRectangle(cornerRadius: 30))
+                    
+                    
+                    NavigationLink(destination: Text("You're logged in @\(username)"), isActive: $showingLoginScreen) {
+                        EmptyView()
+                    }
+                        
+                        
+            
+                        
+                }
+            }
         }
-        .padding()
+        
+    }
+    
+    func authenticateUser(username: String, password: String) {
+        
+        if username.lowercased() == "ejder2024" {
+            wrongUsername = 0
+            
+            if password.lowercased() == "ab123" {
+                wrongPassword = 0
+                showingLoginScreen = true
+            } else {
+                wrongPassword = 5 // border kalınlığı(kırmızı renk)
+            }
+        } else {
+            wrongUsername = 5
+        }
     }
 }
+
+
 
 #Preview {
     ContentView()
